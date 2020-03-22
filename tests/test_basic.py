a36b1d4e0ffd67499d847fd819cfe2f68eb2ca78
millefalcon <hanish0019@gmail.com>
2020-03-22 02:24:06 -0400

    Added test for new implementation of IRC framework

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,6 +2,8 @@ import trio
 
 from irc import IRCBase
 
+from irc_v2 import IRCBase as IRCBase_V2
+
 """
 #async def test_connect_to_irc():
 #    class IRCClient(IRCBase):
@@ -17,27 +19,40 @@ from irc import IRCBase
 
 """
 
+"""
+#async def test_join_channel():
+#    join_happened = False
+#    class IRCClient(IRCBase):
+#        nickname = 'triobot'
+#        def __init__(self, host, port, chan):
+#            super().__init__(host, port, chan)
+#
+#        async def irc_JOIN(self, prefix, params):
+#            global join_happened
+#            join_happaned = True
+#            print('****', prefix, params)
+#            self.logger.info(f"**{prefix}, {params}")
+#            1/0
+#            print('****', "nothing happened")
+#            assert prefix == "[JOIN]"
+#            raise KeyboardInterrupt
+#
+#    host, port = ('irc.freenode.net', 6667)
+#    client = IRCClient(host, port, '#dummychannel')
+#    with trio.move_on_after(60):
+#        await client.connect()
+#    assert join_happened
+"""
+
 async def test_join_channel():
-    join_happened = False
-    class IRCClient(IRCBase):
-        nickname = 'triobot'
-        def __init__(self, host, port, chan):
-            super().__init__(host, port, chan)
-
-        async def irc_JOIN(self, prefix, params):
-            global join_happened
-            join_happaned = True
-            print('****', prefix, params)
-            self.logger.info(f"**{prefix}, {params}")
-            1/0
-            print('****', "nothing happened")
-            assert prefix == "[JOIN]"
-            raise KeyboardInterrupt
-
-    host, port = ('irc.freenode.net', 6667)
-    client = IRCClient(host, port, '#dummychannel')
-    with trio.move_on_after(60):
-        await client.connect()
-        print("join ?", join_happened)
-        if join_happened:
+    host, port, channel = ("irc.freenode.net", 6667, "#bash")
+    irc = IRCBase_V2(host, port)
+    await irc.connect()
+    await irc._join(channel)
+    async for event in irc.events():
+        if event.type == 'ERR_NICKNAMEINUSE':
+            await irc.set_nick(irc.nickname + '_')
+            await irc._join(channel)
+        if event.type == 'JOIN':
+            await irc.disconnect()
             assert True
